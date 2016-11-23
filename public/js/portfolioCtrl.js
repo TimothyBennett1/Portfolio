ec9991287fd0e0b65b9ff117ab06dd2a495ca96d
TimothyBennett1 <timothybennett2323@gmail.com>
2016-11-23 11:48:33 -0600

    trying to host on git pages

--- public/js/portfolioCtrl.js
+++ public/js/portfolioCtrl.js
@@ -1,4 +1,14 @@
 angular.module('portfolioApp')
-  .controller('portfolioCtrl', function() {
+  .controller('portfolioCtrl', function($scope) {
 
+    $scope.open = false;
+
+    $scope.openNav = () => {
+      document.getElementById('small-nav').style.display = "flex";
+      $scope.open = true;
+    }
+    $scope.closeNav = () => {
+      document.getElementById('small-nav').style.display = "none";
+      $scope.open = false;
+    }
   });
